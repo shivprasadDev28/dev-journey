c7f913b4d25508340578041c7d28f7dd4f52621e
ShivPrasad Devkatte <shivprasaddevkattee5@gmail.com>
2025-07-30 21:45:01 +0530

    did some question then left for next day

--- Python/Topics_to_cover.py
+++ Python/Topics_to_cover.py
@@ -11,14 +11,4 @@ loops (for, while)
 nested conditions
 """
 
-# todo Password Checker
-
-rows = int(input("Enter number of rows: "))
-
-print("Pattern 1:")
-for i in range(1, rows + 1):
-    print('*' * i)
-
-print("\nPattern 2:")
-for i in range(rows, 0, -1):
-    print('*' * i)
+# todo Write a program that prints all prime numbers up to a given number.

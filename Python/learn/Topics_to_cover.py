af7415c155f0d315a5d57d1db61e331f73951474
ShivPrasad Devkatte <shivprasaddevkattee5@gmail.com>
2025-08-11 20:16:01 +0530

    leader of array

--- Python/learn/Topics_to_cover.py
+++ Python/learn/Topics_to_cover.py
@@ -17,49 +17,14 @@ Functions
 CLASS
 """
 
-#* current TOPIC HAShing
+num = [10,22,12,3,0,6]
+l = []
+l.append(num[-1])
+maxi = num[-1]
 
-# Input: arr[] = {10,5,10,15,10,5};
-# o/p
-#10 3
-#5 2
-#15 1
-def merge_sort(arr,low,high):
-    if low >= high:
-        return
-    
-    mid = (low+high)//2
-    
-    merge_sort(arr,low,mid) #left
-    merge_sort(arr,mid+1,high) #right
-    merge(arr,low,mid,high)
+for i in range(len(num)-1,-1,-1):
+    if num[i] > maxi:
+        maxi = num[i]
+        l.append(num[i])
 
-def merge(arr,low,mid,high):
-    left = arr[low:mid+1]
-    right = arr[mid+1:high+1]
-
-    i= j = 0
-    k = low
-
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            arr[k] = left[i]
-            i+=1
-        else:
-            arr[k] = right[j]
-            j+=1
-        k+=1
-    
-    while i < len(left):
-        arr[k] = left[i]
-        i+=1
-        k+=1
-    
-    while j < len(right):
-        arr[k] = right[j]
-        j+=1
-        k+=1
-
-num = [3,2,4,1,3]
-merge_sort(num,0,len(num)-1)
-print(num)
+print(l)
